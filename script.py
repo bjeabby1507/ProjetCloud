a881571013358fe824c6abdfac7dc3eee7cb8811
bjeabby1507 <58377788+bjeabby1507@users.noreply.github.com>
2021-10-22 11:13:28 +0200

    Add files via upload

---
+++ script.py
@@ -0,0 +1,30 @@
+import requests
+import json
+import time
+
+# url = "https://bsc-blockbook.nownodes.io/api/v2/xpub/<xpub>[?page=<page>&pageSize=<size>&from=<block height>&to=<block height>&details=<basic|tokens|tokenBalances|txids|txs>&tokens=<nonzero|used|derived>]"
+url = "https://eth-blockbook.nownodes.io/api/"
+
+payload={}
+headers = {
+  'api-key': '0Y3Es9FydORuZAh7pkXITVSqceiGKWtb',
+  'Content-Type': 'application/json'
+}
+
+# response = requests.request("GET", url, headers=headers, data=payload)
+
+# print(response.text)
+
+def getdata(url, headers):
+    try:
+        response = requests.request("GET", url, headers=headers, data=payload)
+        print (response.text)
+        response_json = json.loads(response.text)
+        print(response_json)
+        print (type(response_json))
+        time.sleep(5)
+        getdata(url, headers)
+    except:
+        print ("an error as occured")
+        
+print(getdata(url, headers))
